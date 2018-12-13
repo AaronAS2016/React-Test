f2830f9fcda6be32c2a1d66c5878e8b4742c1e57
Aaron <aaronsaban98@gmail.com>
2018-12-13 16:48:11 -0300

    Manejo de errores

---
+++ src/error/components/regular-error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HandleError from '../containers/handle-error';
+
+function RegularError(props){
+    return(
+        <h1>Ha ocurrido un error D: </h1>
+    )
+}
+
+export default RegularError;
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,6 +3,8 @@ import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal';
+import handleError from '../../error/containers/handle-error';
+import HandleError from '../../error/containers/handle-error';
 
 class Home extends Component{
 
@@ -16,26 +18,24 @@ class Home extends Component{
         })
     }
 
-    componentDidCatch(error, info){
-
-    }
-
     render(){
         return(
-            <HomeLayout>
-                <Categories categories={this.props.data.categories} handleToggleModal = {this.handleToggleModal} />
-                {
-                    this.state.modalVisible &&
-                    <ModalContainer>
-                        <Modal
-                            handleClick = {this.handleToggleModal}
-                        >
-                        <h1>Esto es un portal</h1>
-                        </Modal>
-                    </ModalContainer>
-                }
-              
-            </HomeLayout>
+            <HandleError>
+                <HomeLayout>
+                    <Categories categories={this.props.data.categories} handleToggleModal = {this.handleToggleModal} />
+                    {
+                        this.state.modalVisible &&
+                        <ModalContainer>
+                            <Modal
+                                handleClick = {this.handleToggleModal}
+                            >
+                            <h1>Esto es un portal</h1>
+                            </Modal>
+                        </ModalContainer>
+                    }
+                
+                </HomeLayout>
+            </HandleError>
         )
     }
 }
